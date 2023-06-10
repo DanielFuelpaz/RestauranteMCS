2b63f7cd02d8a106a5f4f8b36733bd056be1015a
D4rk Gh057 <efuelpaz8870@uta.edu.ec>
2023-06-10 16:37:27 -0500

    se añaden mas funciones en main.js

--- main.js
+++ main.js
@@ -38,3 +38,24 @@ words2.forEach((word, index) => {
     heading2.appendChild(space);
   }
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Obtener todos los elementos de navegación
+  var navItems = document.querySelectorAll(".navbar-nav .nav-link");
+
+  // Agregar el evento de clic a cada elemento de navegación
+  navItems.forEach(function(item) {
+    item.addEventListener("click", function() {
+      // Verificar si ya existe un elemento activo
+      var activeItem = document.querySelector(".navbar-nav .nav-link.active");
+
+      // Desactivar el elemento activo si existe
+      if (activeItem) {
+        activeItem.classList.remove("active");
+      }
+
+      // Activar el elemento clicado
+      this.classList.add("active");
+    });
+  });
+});
